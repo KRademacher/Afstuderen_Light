e924de355ab550e4ab40220170cddc4257558f79
KRademacher <Kevin_Rademacher@hotmail.com>
2020-06-14 17:21:25 +0200

    Cleaned up code, fixed a cast bug

--- Server/WeatherData.cs
+++ Server/WeatherData.cs
@@ -11,5 +11,59 @@ namespace Server
         public string WindDirection { get; set; }
         public float WindSpeed { get; set; }
         public int AtmosphericPressure { get; set; }
+
+        private readonly Random random = new Random();
+
+        public WeatherData()
+        {
+            Timestamp = Timestamp.FromDateTime(DateTime.UtcNow);
+            Temperature = (float)random.Next(200, 281) / 10;
+            Humidity = (float)random.Next(700, 1001) / 10;
+            WindDirection = PickWindDirection(random.Next(1, 17));
+            WindSpeed = (float)random.Next(100, 201) / 10;
+            AtmosphericPressure = random.Next(800, 1001);
+        }
+
+        //Don't look at this, it's ugly.
+        private string PickWindDirection(int i)
+        {
+            switch (i)
+            {
+                case 1:
+                    return "N";
+                case 2:
+                    return "NNE";
+                case 3:
+                    return "NE";
+                case 4:
+                    return "ENE";
+                case 5:
+                    return "E";
+                case 6:
+                    return "ESE";
+                case 7:
+                    return "SE";
+                case 8:
+                    return "SSE";
+                case 9:
+                    return "S";
+                case 10:
+                    return "SSW";
+                case 11:
+                    return "SW";
+                case 12:
+                    return "WSW";
+                case 13:
+                    return "W";
+                case 14:
+                    return "WNW";
+                case 15:
+                    return "NW";
+                case 16:
+                    return "NNW";
+                default:
+                    return "";
+            }
+        }
     }
 }
--- Server_gRPC/Program.cs
+++ Server_gRPC/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Threading;
-using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 using Server;
 using Weatherstation;
@@ -10,9 +8,6 @@ namespace Server_gRPC
     class Program
     {
         const int port = 50051;
-        static readonly Random random = new Random();
-        static TimeSpan startTimeSpan = TimeSpan.Zero;
-        static TimeSpan secondTimeSpan = TimeSpan.FromSeconds(1);
         private static System.Timers.Timer timer;
 
         static void Main(string[] args)
@@ -36,14 +31,14 @@ namespace Server_gRPC
             // Create a timer with a one second interval.
             timer = new System.Timers.Timer(1000);
             // Hook up the Elapsed event for the timer. 
-            timer.Elapsed += (sender, e) => TimerElapsed(sender, e, client);
+            timer.Elapsed += (sender, e) => OnTimerElapsed(sender, e, client);
             timer.AutoReset = true;
             timer.Enabled = true;
         }
 
-        private static void TimerElapsed(object sender, System.Timers.ElapsedEventArgs e, WeatherDataSender.WeatherDataSenderClient client)
+        private static void OnTimerElapsed(object sender, System.Timers.ElapsedEventArgs e, WeatherDataSender.WeatherDataSenderClient client)
         {
-            WeatherData weatherData = GenerateWeatherData(random);
+            WeatherData weatherData = new WeatherData();
 
             DataReply response = client.SendData(new DataRequest
             {
@@ -56,63 +51,6 @@ namespace Server_gRPC
             });
 
             Console.WriteLine("Response: " + response.Message);
-        }
-
-        static private WeatherData GenerateWeatherData(Random random)
-        {
-            WeatherData weatherData = new WeatherData()
-            {
-                Timestamp = Timestamp.FromDateTime(DateTime.UtcNow),
-                Temperature = (random.Next(250, 261) / 10),
-                Humidity = (random.Next(700, 1001) / 10),
-                WindDirection = PickWindDirection(random.Next(1, 17)),
-                WindSpeed = (random.Next(100, 201) / 10),
-                AtmosphericPressure = random.Next(800, 1001)
-            };
-
-            return weatherData;
-        }
-
-        //Don't look at this, it's ugly.
-        static private string PickWindDirection(int i)
-        {
-            switch (i)
-            {
-                case 1:
-                    return "N";
-                case 2:
-                    return "NNE";
-                case 3:
-                    return "NE";
-                case 4:
-                    return "ENE";
-                case 5:
-                    return "E";
-                case 6:
-                    return "ESE";
-                case 7:
-                    return "SE";
-                case 8:
-                    return "SSE";
-                case 9:
-                    return "S";
-                case 10:
-                    return "SSW";
-                case 11:
-                    return "SW";
-                case 12:
-                    return "WSW";
-                case 13:
-                    return "W";
-                case 14:
-                    return "WNW";
-                case 15:
-                    return "NW";
-                case 16:
-                    return "NNW";
-                default:
-                    return "";
-            }
         }
     }
 }
